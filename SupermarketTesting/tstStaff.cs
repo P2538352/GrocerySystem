2049d9b42bc3a7c9aa11c8f63d87c60305b0d001
Komal Shahzadi <P2546670@my365.dmu.ac.uk>
2022-03-19 12:32:48 +0000

    Add testing for staff class

---
+++ SupermarketTesting/tstStaff.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SupermarketTesting
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void Instance()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //test to see that it exists
+            Assert.IsNotNull(AStaff);
+        }
+    }
+
+    class clsStaff
+    {
+    }
+}
