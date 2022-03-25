7b6d48ed1f92438493ccc498ccbb89d9890d77e7
dshaun williams <P2601523@my365.dmu.ac.uk>
2022-03-25 02:29:06 +0000

    added testing for deliveries class

---
+++ SupermarketTesting/tstDeliveries.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SupermarketTesting
+{
+    [TestClass]
+    public class tstDeliveries
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //creates an instance of class we want to create
+            clsDeliveries AnDeliveries = new clsDeliveries();
+        }
+    }
+
+    class clsDeliveries
+    {
+    }
+}
